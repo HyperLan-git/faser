659c12ba6dfbee81d1eb3652677a9abf95fe1d0f
HyperLan-git <hyperlanf@gmail.com>
2024-09-02 15:42:45 +0200

    windose work

--- Fased/Source/FreqResponseComponent.cpp
+++ Fased/Source/FreqResponseComponent.cpp
@@ -1,15 +1,15 @@
 #include "FreqResponseComponent.hpp"
 
 FreqResponseComponent::FreqResponseComponent(
-    const BiquadFilter& filter, juce::AudioParameterInt* filterParam,
-    ResponseCallback&& callback)
-    : filter(filter), filterParam(filterParam), callback(callback) {
-    setSize(400, 400);
+	const BiquadFilter& filter, juce::AudioParameterInt* filterParam,
+	ResponseCallback&& callback)
+	: filter(filter), filterParam(filterParam), callback(callback) {
+	setSize(400, 400);
 }
 
 FreqResponseComponent::~FreqResponseComponent() { this->callback = nullptr; }
 
-constexpr double a = std::log10(MAX_FREQ), b = std::log10(40);
+const double a = std::log10(MAX_FREQ), b = std::log10(40);
 
 constexpr float MAX_DB = 40;
 
@@ -18,110 +18,111 @@ typedef std::complex<double> complex;
 constexpr complex ONE(1.);
 
 void FreqResponseComponent::paint(juce::Graphics& g) {
-    using namespace std::complex_literals;
+	using namespace std::complex_literals;
 
-    BiquadFilterCoefficients c = filter.getCoeffs();
-    int filters = *filterParam;
-    int w = this->getWidth(), h = this->getHeight();
-    if (w > MAX_WIDTH) w = MAX_WIDTH;
-    if (w <= 1) return;
-    if (c != this->lastCoeffs || filters != this->lastFilters) {
-        complex a0(c.a0), a1(c.a1), a2(c.a2), b0(c.b0), b1(c.b1), b2(c.b2);
+	BiquadFilterCoefficients c = filter.getCoeffs();
+	int filters = *filterParam;
+	int w = this->getWidth(), h = this->getHeight();
+	if (w > MAX_WIDTH) w = MAX_WIDTH;
+	if (w <= 1) return;
+	if (c != this->lastCoeffs || filters != this->lastFilters) {
+		complex a0(c.a0), a1(c.a1), a2(c.a2), b0(c.b0), b1(c.b1), b2(c.b2);
 
-        for (int i = 0; i < w; i++) {
-            double f = std::pow(10, i * (a - b) / (w - 1) + b);
-            complex z = std::__complex_exp<double>(complex(
-                        0, juce::MathConstants<double>::pi * f / MAX_FREQ)),
-                    z1 = ONE / z, z2 = ONE / (z * z);
+		for (int i = 0; i < w; i++) {
+			double f = std::pow(10, i * (a - b) / (w - 1) + b);
+			complex z = std::exp<double>(complex(
+				0, juce::MathConstants<double>::pi * f / MAX_FREQ)),
+				z1 = ONE / z, z2 = ONE / (z * z);
 
-            values[i] = (b0 + b1 * z1 + b2 * z2) / (a0 + a1 * z1 + a2 * z2);
-            if (filters > 1) values[i] = std::pow(values[i], filters);
-        }
-        this->lastFilters = filters;
-        this->lastCoeffs = c;
-    }
+			values[i] = (b0 + b1 * z1 + b2 * z2) / (a0 + a1 * z1 + a2 * z2);
+			if (filters > 1) values[i] = std::pow(values[i], filters);
+		}
+		this->lastFilters = filters;
+		this->lastCoeffs = c;
+	}
 
-    g.fillAll(juce::Colours::black);
+	g.fillAll(juce::Colours::black);
 
-    juce::Path p, p2;
+	juce::Path p, p2;
 
-    p.preallocateSpace(1 + (w + 4) * 3);
-    p.startNewSubPath({-200, h / 2.f});
+	p.preallocateSpace(1 + (w + 4) * 3);
+	p.startNewSubPath({ -200, h / 2.f });
 
-    p2.preallocateSpace(1 + (w + 4) * 3);
-    p2.startNewSubPath({-200, h / 2.f});
-    for (int x = 0; x < w; x++) {
-        if (values[x].real() == 0 && values[x].imag() == 0) {
-            p.lineTo(x, h + 20);
-            p2.lineTo(x, h + 20);
-            continue;
-        }
-        const double mag = (this->filter.getType() == ALLPASS)
-                               ? 0
-                               : 20 * std::log10(std::abs(values[x]));
-        const double phase = std::arg(values[x]);
-        const int y = (int)(h / 2 - h * mag / MAX_DB / 2);
-        const int y2 =
-            (int)(h / 2 - h * phase / juce::MathConstants<double>::pi / 2);
-        p.lineTo(x, y);
-        if (y > 2 * h)
-            p2.lineTo(x, h + 20);
-        else
-            p2.lineTo(x, y2);
-    }
-    p.lineTo(w + 200, h / 2.f);
-    p.lineTo(w, h + 20);
-    p.lineTo(-200, h + 20);
-    p.closeSubPath();
+	p2.preallocateSpace(1 + (w + 4) * 3);
+	p2.startNewSubPath({ -200, h / 2.f });
+	for (int x = 0; x < w; x++) {
+		if (values[x].real() == 0 && values[x].imag() == 0) {
+			p.lineTo((float)x, h + 20.f);
+			p2.lineTo((float)x, h + 20.f);
+			continue;
+		}
+		const double mag = (this->filter.getType() == ALLPASS)
+			? 0
+			: 20 * std::log10(std::abs(values[x]));
+		const double phase = std::arg(values[x]);
+		const int y = (int)(h / 2 - h * mag / MAX_DB / 2);
+		const int y2 =
+			(int)(h / 2 - h * phase / juce::MathConstants<double>::pi / 2);
+		p.lineTo((float)x, (float)y);
+		if (y > 2 * h)
+			p2.lineTo((float)x, (float)h + 20);
+		else
+			p2.lineTo((float)x, (float)y2);
+	}
+	p.lineTo((float)w + 200, h / 2.f);
+	p.lineTo((float)w, (float)h + 20);
+	p.lineTo((float)-200, (float)h + 20);
+	p.closeSubPath();
 
-    p2.lineTo(w + 200, h / 2.f);
-    p2.lineTo(w, h + 20);
-    p2.lineTo(-200, h + 20);
-    p2.closeSubPath();
+	p2.lineTo((float)w + 200, (float)h / 2.f);
+	p2.lineTo((float)w, (float)h + 20);
+	p2.lineTo((float)-200, (float)h + 20);
+	p2.closeSubPath();
 
-    g.setColour(juce::Colours::orange);
-    g.fillPath(p);
-    g.setColour(juce::Colours::orange.darker());
-    g.strokePath(p, juce::PathStrokeType(5),
-                 juce::AffineTransform().withAbsoluteTranslation(0, 3));
+	g.setColour(juce::Colours::orange);
+	g.fillPath(p);
+	g.setColour(juce::Colours::orange.darker());
+	g.strokePath(p, juce::PathStrokeType(5),
+		juce::AffineTransform().withAbsoluteTranslation(0, 3));
 
-    g.setColour(juce::Colours::cyan.withAlpha(.1f));
-    g.fillPath(p2);
-    g.setColour(juce::Colours::cyan.darker().withAlpha(.1f));
-    g.strokePath(p2, juce::PathStrokeType(5),
-                 juce::AffineTransform().withAbsoluteTranslation(0, 3));
+	g.setColour(juce::Colours::cyan.withAlpha(.1f));
+	g.fillPath(p2);
+	g.setColour(juce::Colours::cyan.darker().withAlpha(.1f));
+	g.strokePath(p2, juce::PathStrokeType(5),
+		juce::AffineTransform().withAbsoluteTranslation(0, 3));
 }
 
 float getFreq(float posX, int w) {
-    return std::pow(10, posX * (a - b) / (w - 1) + b);
+	return std::pow(10, (float)(posX * (a - b) / (w - 1) + b));
 }
 
 float getGain(float posY, int h) { return MAX_DB - posY * MAX_DB * 2 / h; }
 
 void FreqResponseComponent::mouseDown(const juce::MouseEvent& event) {
-    float freq = getFreq(event.position.x, this->getWidth()),
-          gain = getGain(event.position.y, this->getHeight());
-    auto callback = this->callback;
-    if (callback)
-        callback(std::make_optional<float>(freq),
-                 std::make_optional<float>(gain), std::optional<float>());
+	float freq = getFreq(event.position.x, this->getWidth()),
+		gain = getGain(event.position.y, this->getHeight());
+	auto cb = this->callback;
+	if (cb)
+		cb(std::make_optional<float>(freq),
+			std::make_optional<float>(gain), std::optional<float>());
 }
 void FreqResponseComponent::mouseDrag(const juce::MouseEvent& event) {
-    float freq = getFreq(event.position.x, this->getWidth()),
-          gain = getGain(event.position.y, this->getHeight());
-    auto callback = this->callback;
-    if (callback)
-        callback(std::make_optional<float>(freq),
-                 std::make_optional<float>(gain), std::optional<float>());
+	float freq = getFreq(event.position.x, this->getWidth()),
+		gain = getGain(event.position.y, this->getHeight());
+	auto callback = this->callback;
+	if (callback)
+		callback(std::make_optional<float>(freq),
+			std::make_optional<float>(gain), std::optional<float>());
 }
 
 void FreqResponseComponent::mouseWheelMove(
-    const juce::MouseEvent& event, const juce::MouseWheelDetails& wheel) {
-    auto callback = this->callback;
-    if (callback)
-        callback(std::optional<float>(), std::optional<float>(),
-                 std::make_optional<float>(wheel.isReversed ? -wheel.deltaY
-                                                            : wheel.deltaY));
+	const juce::MouseEvent& event, const juce::MouseWheelDetails& wheel) {
+	(void)event;
+	auto callback = this->callback;
+	if (callback)
+		callback(std::optional<float>(), std::optional<float>(),
+			std::make_optional<float>(wheel.isReversed ? -wheel.deltaY
+				: wheel.deltaY));
 }
 
 void FreqResponseComponent::resized() {}
--- Fased/Source/PluginProcessor.cpp
+++ Fased/Source/PluginProcessor.cpp
@@ -2,62 +2,62 @@
 
 FasedAudioProcessor::FasedAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-    : AudioProcessor(
-          BusesProperties()
+	: AudioProcessor(
+		BusesProperties()
 #if !JucePlugin_IsMidiEffect
 #if !JucePlugin_IsSynth
-              .withInput("Input", juce::AudioChannelSet::stereo(), true)
+		.withInput("Input", juce::AudioChannelSet::stereo(), true)
 #endif
-              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+		.withOutput("Output", juce::AudioChannelSet::stereo(), true)
 #endif
-              )
+	)
 #endif
-      ,
-      freq(new juce::AudioParameterFloat({"frequency", 1}, "Frequency", 5,
-                                         20000, 500)),
-      Q(new juce::AudioParameterFloat({"Q", 1}, "Q value", .01f, 20, .707f)),
-      gain(new juce::AudioParameterFloat({"gain", 1}, "Gain", -30, 30, 6)),
-      filters(new juce::AudioParameterInt({"filters", 1}, "Filter slope", 1,
-                                          MAX_FILTERS, 1)),
-      type(new juce::AudioParameterChoice(
-          {"type", 1}, "Filter type",
-          {"Allpass", "Lowpass", "Highpass", "Bandpass", "Notch", "Peak",
-           "Lowshelf", "Highshelf"},
-          1)) {
-    addParameter(freq);
-    addParameter(Q);
-    addParameter(gain);
-    addParameter(type);
-    addParameter(filters);
+	,
+	freq(new juce::AudioParameterFloat({ "frequency", 1 }, "Frequency", 5,
+		20000, 500)),
+	Q(new juce::AudioParameterFloat({ "Q", 1 }, "Q value", .01f, 20, .707f)),
+	gain(new juce::AudioParameterFloat({ "gain", 1 }, "Gain", -30, 30, 6)),
+	filters(new juce::AudioParameterInt({ "filters", 1 }, "Filter slope", 1,
+		MAX_FILTERS, 1)),
+	type(new juce::AudioParameterChoice(
+		{ "type", 1 }, "Filter type",
+		{ "Allpass", "Lowpass", "Highpass", "Bandpass", "Notch", "Peak",
+		 "Lowshelf", "Highshelf" },
+		1)) {
+	addParameter(freq);
+	addParameter(Q);
+	addParameter(gain);
+	addParameter(type);
+	addParameter(filters);
 }
 
 FasedAudioProcessor::~FasedAudioProcessor() {}
 
 const juce::String FasedAudioProcessor::getName() const {
-    return JucePlugin_Name;
+	return JucePlugin_Name;
 }
 
 bool FasedAudioProcessor::acceptsMidi() const {
 #if JucePlugin_WantsMidiInput
-    return true;
+	return true;
 #else
-    return false;
+	return false;
 #endif
 }
 
 bool FasedAudioProcessor::producesMidi() const {
 #if JucePlugin_ProducesMidiOutput
-    return true;
+	return true;
 #else
-    return false;
+	return false;
 #endif
 }
 
 bool FasedAudioProcessor::isMidiEffect() const {
 #if JucePlugin_IsMidiEffect
-    return true;
+	return true;
 #else
-    return false;
+	return false;
 #endif
 }
 
@@ -70,134 +70,134 @@ int FasedAudioProcessor::getCurrentProgram() { return 0; }
 void FasedAudioProcessor::setCurrentProgram(int index) { (void)index; }
 
 const juce::String FasedAudioProcessor::getProgramName(int index) {
-    (void)index;
-    return {};
+	(void)index;
+	return {};
 }
 
 void FasedAudioProcessor::changeProgramName(int index,
-                                            const juce::String& newName) {
-    (void)index;
-    (void)newName;
+	const juce::String& newName) {
+	(void)index;
+	(void)newName;
 }
 
 void FasedAudioProcessor::prepareToPlay(double sampleRate,
-                                        int samplesPerBlock) {
-    (void)sampleRate;
-    (void)samplesPerBlock;
+	int samplesPerBlock) {
+	(void)sampleRate;
+	(void)samplesPerBlock;
 }
 
 void FasedAudioProcessor::releaseResources() {}
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool FasedAudioProcessor::isBusesLayoutSupported(
-    const BusesLayout& layouts) const {
+	const BusesLayout& layouts) const {
 #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused(layouts);
-    return true;
+	juce::ignoreUnused(layouts);
+	return true;
 #else
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
-        layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+		layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+		return false;
 
 #if !JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
+	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+		return false;
 #endif
 
-    return true;
+	return true;
 #endif
 }
 #endif
 
 void FasedAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
-                                       juce::MidiBuffer& midiMessages) {
-    (void)midiMessages;
+	juce::MidiBuffer& midiMessages) {
+	(void)midiMessages;
 
-    juce::ScopedNoDenormals noDenormals;
-    const int inputs = getTotalNumInputChannels();
-    const int outputs = getTotalNumOutputChannels();
+	juce::ScopedNoDenormals noDenormals;
+	const int inputs = getTotalNumInputChannels();
+	const int outputs = getTotalNumOutputChannels();
 
-    if (inputs == 0) return;
-    if (outputs == 0) return;
+	if (inputs == 0) return;
+	if (outputs == 0) return;
 
-    const int samples = buffer.getNumSamples();
-    const double rate = getSampleRate();
+	const int samples = buffer.getNumSamples();
+	const double rate = getSampleRate();
 
-    if (rate != filter.getSampleRate()) filter.setSampleRate(rate);
+	if (rate != filter.getSampleRate()) filter.setSampleRate(rate);
 
-    const float* l = buffer.getWritePointer(0);
-    const float* r =
-        inputs < 2 || outputs < 2 ? nullptr : buffer.getWritePointer(1);
+	float* l = buffer.getWritePointer(0);
+	float* r =
+		inputs < 2 || outputs < 2 ? nullptr : buffer.getWritePointer(1);
 
-    const int n = *filters;
-    float fc = *freq;
-    float q = *Q;
-    float g = (*gain) / n;
-    enum BiquadFilterType t =
-        static_cast<enum BiquadFilterType>(type->getIndex() + 1);
+	const int n = *filters;
+	float fc = *freq;
+	float q = *Q;
+	float g = (*gain) / n;
+	enum BiquadFilterType t =
+		static_cast<enum BiquadFilterType>(type->getIndex() + 1);
 
-    if (t != filter.getType())
-        std::memset(this->states, 0, 2 * MAX_FILTERS * sizeof(struct SOState));
+	if (t != filter.getType())
+		std::memset(this->states, 0, 2 * MAX_FILTERS * sizeof(struct SOState));
 
-    BiquadFilterParams b = filter.getParameters();
-    if (t != filter.getType() || b.f != fc || b.Q != q || b.gain != g ||
-        prevFilters != n) {
-        if (prevFilters != n && n < MAX_FILTERS) {
-            std::memset(this->states + n + 1, 0,
-                        sizeof(struct SOState) * (MAX_FILTERS - n - 1));
-            std::memset(this->states + MAX_FILTERS + n, 0,
-                        sizeof(struct SOState) * (MAX_FILTERS - n - 1));
-        }
-        filter.setParameters(t, {.f = fc, .Q = q, .gain = g});
-        juce::AudioProcessorEditor* editor = this->getActiveEditor();
-        if (editor) {
-            juce::MessageManager::callAsync(
-                [editor]() { editor->repaint(editor->getBounds()); });
-        }
-    }
+	BiquadFilterParams b = filter.getParameters();
+	if (t != filter.getType() || b.f != fc || b.Q != q || b.gain != g ||
+		prevFilters != n) {
+		if (prevFilters != n && n < MAX_FILTERS) {
+			std::memset(this->states + n + 1, 0,
+				sizeof(struct SOState) * (MAX_FILTERS - n - 1));
+			std::memset(this->states + MAX_FILTERS + n, 0,
+				sizeof(struct SOState) * (MAX_FILTERS - n - 1));
+		}
+		filter.setParameters(t, { fc, q, g });
+		juce::AudioProcessorEditor* editor = this->getActiveEditor();
+		if (editor) {
+			juce::MessageManager::callAsync(
+				[editor]() { editor->repaint(editor->getBounds()); });
+		}
+	}
 
-    // TODO think of an optimisation since y1 and y2 of a filter is equal to x1
-    // and x2 of the next
-    for (int i = 0; i < n; i++) {
-        struct SOState &left = this->states[i],
-                       &right = this->states[i + MAX_FILTERS];
-        filter.processBlock(l, samples, left);
-        if (r) filter.processBlock(r, samples, right);
-    }
-    this->prevFilters = n;
+	// TODO think of an optimisation since y1 and y2 of a filter is equal to x1
+	// and x2 of the next
+	for (int i = 0; i < n; i++) {
+		struct SOState& left = this->states[i],
+			& right = this->states[i + MAX_FILTERS];
+		filter.processBlock(l, samples, left);
+		if (r) filter.processBlock(r, samples, right);
+	}
+	this->prevFilters = n;
 }
 
 void FasedAudioProcessor::setFilterType(enum BiquadFilterType type) {
-    this->type->setValueNotifyingHost(
-        this->type->convertTo0to1(static_cast<float>(type)));
+	this->type->setValueNotifyingHost(
+		this->type->convertTo0to1(static_cast<float>(type)));
 }
 
 bool FasedAudioProcessor::hasEditor() const { return true; }
 
 juce::AudioProcessorEditor* FasedAudioProcessor::createEditor() {
-    return new FasedAudioProcessorEditor(*this);
+	return new FasedAudioProcessorEditor(*this);
 }
 
 void FasedAudioProcessor::getStateInformation(juce::MemoryBlock& destData) {
-    juce::MemoryOutputStream stream(destData, true);
-    stream.writeFloat(GET_PARAM_NORMALIZED(freq));
-    stream.writeFloat(GET_PARAM_NORMALIZED(Q));
-    stream.writeFloat(GET_PARAM_NORMALIZED(gain));
-    stream.writeInt(*filters);
-    stream.writeInt(GET_PARAM_NORMALIZED(type));
+	juce::MemoryOutputStream stream(destData, true);
+	stream.writeFloat(GET_PARAM_NORMALIZED(freq));
+	stream.writeFloat(GET_PARAM_NORMALIZED(Q));
+	stream.writeFloat(GET_PARAM_NORMALIZED(gain));
+	stream.writeInt(*filters);
+	stream.writeInt(GET_PARAM_NORMALIZED(type));
 }
 
 void FasedAudioProcessor::setStateInformation(const void* data,
-                                              int sizeInBytes) {
-    juce::MemoryInputStream stream(data, static_cast<size_t>(sizeInBytes),
-                                   false);
-    freq->setValueNotifyingHost(stream.readFloat());
-    Q->setValueNotifyingHost(stream.readFloat());
-    gain->setValueNotifyingHost(stream.readFloat());
-    filters->setValueNotifyingHost(filters->convertTo0to1(stream.readInt()));
-    type->setValueNotifyingHost(stream.readInt());
+	int sizeInBytes) {
+	juce::MemoryInputStream stream(data, static_cast<size_t>(sizeInBytes),
+		false);
+	freq->setValueNotifyingHost(stream.readFloat());
+	Q->setValueNotifyingHost(stream.readFloat());
+	gain->setValueNotifyingHost(stream.readFloat());
+	filters->setValueNotifyingHost(filters->convertTo0to1(stream.readInt()));
+	type->setValueNotifyingHost(stream.readInt());
 }
 
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
-    return new FasedAudioProcessor();
+	return new FasedAudioProcessor();
 }
